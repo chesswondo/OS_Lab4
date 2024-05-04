cc26f45e09585b7edfb8ebef9b4c4e3313e9c971
chesswondo <chesswondo@gmail.com>
2024-05-05 00:08:38 +0300

    Added report and some comments

--- memprog.cpp
+++ memprog.cpp
@@ -6,16 +6,19 @@
 
 using namespace std;
 
+// String to change and read
 char str[64] = "Now is ";
 
 int main()
 {
+    // Output mem info 
     cout <<
         "PID:    " << getpid()    << "\n" <<
         "ADDR:   " << (void*)str  << "\n" <<
         "SIZE:   " << sizeof(str) << "\n";
     cout << "You are welcome!\n";
 
+    // Change string every 1s afres 20s delay
     for(this_thread::sleep_for(20s);;this_thread::sleep_for(1s))
     {
         time_t now = time(0);
--- memview.cpp
+++ memview.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
     string pid, s;
     unsigned long long addr, size;
-
+    // Get info about memory
     cout << "Pid:   ";
     cin >> pid;
     cout << "Addr:  ";
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
     cout << "Size:  ";
     cin >> size;
 
+    // device name
     string memname = "/proc/" + pid + "/mem";
     int fd = open(memname.c_str(), O_RDONLY);
     if(fd == -1)
@@ -25,19 +26,23 @@ int main(int argc, char **argv)
         cerr << "Sorry, error open memory. Try as root\n";
         return -2;
     }
-
+    // Every 1s read mem and output (if it changes)
     for(pid = "", s = string(size,' ');;this_thread::sleep_for(1s))
     {
+        // Seek to required position
         off_t res = lseek(fd, (off_t)addr, SEEK_SET);
         if(res == (off_t)-1)
         {
             cerr << "Sorry, error seek memory\n";
             return -3;
         }
+        // read chunk
         int readed = read(fd, s.data(), s.size());
+        // Limit string
         auto p = s.find('\n');
         if ( p != s.npos) s[p] = 0;
 
+        // Output if different
         if (pid != s)
         {
             pid = s;
